3a5f569262273f51dd8499c7076806e98c701c13
xnousnow <xnou.snow@gmail.com>
2023-03-26 20:53:40 +0900

    :lipstick: 메뉴바 추가

---
+++ src/components/MenuBar.svelte
@@ -0,0 +1,3 @@
+<header class="bg-green-500 py-4 px-5 flex rounded-b-lg text-white shadow-lg">
+  <h1 class="text-[20px] font-semibold">오늘뭐먹지</h1>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<h1>Hello, world!</h1>
+<script lang="ts">
+  import MenuBar from '../components/MenuBar.svelte'
+</script>
+
+<MenuBar />
