2c38685d04ede118a012a12188679b21a62b376f
xnousnow <xnou.snow@gmail.com>
2023-03-27 18:10:25 +0900

    :lipstick: 급식 목록 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
   import MenuBar from '../components/MenuBar.svelte'
+  import MealList from '../components/MealList.svelte'
+
+  let meal = [
+    '쌀보리밥',
+    '설렁탕',
+    '코다리살조림',
+    '연근조림',
+    '딸기',
+    '깍두기'
+  ]
 </script>
 
 <MenuBar />
+<MealList {meal} />
