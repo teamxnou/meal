590389b6a2d63089568cc195fd85aca8300718af
xnousnow <xnou.snow@gmail.com>
2023-03-28 12:31:17 +0900

    :sparkles: 메뉴바에 뒤로가기 기능 추가

--- src/components/MenuBar.svelte
+++ src/components/MenuBar.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
-  export let title: string, buttons: any[]
+  import { ChevronLeft } from 'lucide-svelte'
+
+  export let title: string,
+    buttons: any[] = [],
+    back: boolean = false
 </script>
 
-<header
-  class="flex items-center justify-between rounded-b-lg bg-green-500 py-3 px-5 text-white shadow-lg"
->
+<header class="flex items-center rounded-b-lg bg-green-500 py-3 px-5 text-white shadow-lg">
+  {#if back}
+    <button on:click={() => history.back()} class="mr-3">
+      <ChevronLeft class="h-7 w-7" />
+    </button>
+  {/if}
   <h1 class="text-[20px] font-semibold">{title}</h1>
   {#if buttons.length}
-    <div class="flex h-full items-center">
+    <div class="flex h-full grow items-center justify-end">
       {#each buttons as Button}
         <button class="rounded p-1 hover:bg-black/5 active:bg-black/10">
           <Button class="h-7 w-7" />
