74ec989670c0ce065992da13a4ad430ac63603fd
xnousnow <xnou.snow@gmail.com>
2023-03-28 18:30:53 +0900

    :bug: 학교 검색 결과 카드가 가로로 배치되는 버그 수정

--- src/routes/school-selection/+page.svelte
+++ src/routes/school-selection/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <MenuBar title="학교 선택" back={true} search={true} searchPlaceholder="초등학교 검색" />
-<ul class="flex grow items-start gap-4 bg-neutral-100 p-4">
+<ul class="flex flex-col grow items-start gap-4 bg-neutral-100 p-4">
   <li class="flex w-full flex-col items-start rounded-lg bg-white">
     <span class="mx-5 mt-4 text-neutral-400 text-sm font-medium">서울특별시 서대문구 경기대로9길 10</span>
     <h2 class="mx-5 text-2xl font-semibold">경기초등학교</h2>
