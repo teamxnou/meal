c57009ec73177e5e1929a94d4b3ccab2a6fa2590
xnousnow <xnou.snow@gmail.com>
2023-03-28 10:13:09 +0900

    :art: 코드 포맷

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,15 @@
   import MealList from '../components/MealList.svelte'
   import DatePicker from '../components/DatePicker.svelte'
 
-  let meal = [
-    '쌀보리밥',
-    '설렁탕',
-    '코다리살조림',
-    '연근조림',
-    '딸기',
-    '깍두기'
-  ]
+  let meal = ['쌀보리밥', '설렁탕', '코다리살조림', '연근조림', '딸기', '깍두기']
   let date = new Date().toISOString().slice(0, 10)
 </script>
 
 <MenuBar />
 <MealList {meal} />
-<DatePicker {date} updateDate={(newDate) => {date = newDate}} />
+<DatePicker
+  {date}
+  updateDate={(newDate) => {
+    date = newDate
+  }}
+/>
