cd6729c9bebd73b97df3c826054f2a9d32eb4eb0
xnousnow <xnou.snow@gmail.com>
2023-03-28 09:02:49 +0900

    :lipstick: 날짜 선택기 추가

---
+++ src/components/DatePicker.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { ChevronLeft, ChevronRight } from 'lucide-svelte'
+
+  export let date: string
+  })
+</script>
+
+<div class="mb-16 flex items-center justify-center gap-1">
+  <button>
+    <ChevronLeft class="h-7 w-7" />
+  </button>
+  <input type="date" bind:value={date} class="rounded-lg bg-neutral-50 px-2 py-1 text-center relative" />
+  <button>
+    <ChevronRight class="h-7 w-7" />
+  </button>
+</div>
+
+<style>
+  input[type='date']::-webkit-calendar-picker-indicator {
+    background: transparent;
+    bottom: 0;
+    color: transparent;
+    cursor: pointer;
+    height: auto;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: auto;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import MenuBar from '../components/MenuBar.svelte'
   import MealList from '../components/MealList.svelte'
+  import DatePicker from '../components/DatePicker.svelte'
 
   let meal = [
     '쌀보리밥',
@@ -10,7 +11,9 @@
     '딸기',
     '깍두기'
   ]
+  let date = new Date().toISOString().slice(0, 10)
 </script>
 
 <MenuBar />
 <MealList {meal} />
+<DatePicker {date} />
