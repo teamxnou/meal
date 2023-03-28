57560cf64d9d23cefd7fe78ce6d2396f86caecbc
xnousnow <xnou.snow@gmail.com>
2023-03-28 18:07:36 +0900

    :sparkles: 메뉴바에 검색 기능 추가

--- src/routes/school-selection/+page.svelte
+++ src/routes/school-selection/+page.svelte
@@ -2,4 +2,9 @@
   import MenuBar from '../../components/MenuBar.svelte'
 </script>
 
-<MenuBar title="학교 선택" back={true} />
+<MenuBar
+  title="학교 선택"
+  back={true}
+  search={true}
+  searchPlaceholder="초등학교 검색"
+/>
