d62e948891ca3afead7d0b2c6ecfca31bb23f18d
xnousnow <xnou.snow@gmail.com>
2023-03-29 21:57:15 +0900

    :construction: 날짜 선택 버튼에 `input` 대신 `button` 사용

--- src/components/DatePicker.svelte
+++ src/components/DatePicker.svelte
@@ -18,35 +18,21 @@
 </script>
 
 <div class="mb-16 flex items-center justify-center gap-1">
-  <button on:click={() => changeDate(-1)}>
+  <button class="arrow-button" on:click={() => changeDate(-1)}>
     <ChevronLeft class="h-7 w-7" />
   </button>
-  <label
-    for="datepicker"
+  <button
     class="relative w-[8rem] gap-0 rounded-lg bg-neutral-100 px-3 py-1 text-center"
   >
-    <input type="date" bind:value={date} id="datepicker" class="h-0 w-0" />
-    <span>{formattedDate}</span>
-  </label>
-  <button on:click={() => changeDate(1)}>
+    {formattedDate}
+  </button>
+  <button class="arrow-button" on:click={() => changeDate(1)}>
     <ChevronRight class="h-7 w-7" />
   </button>
 </div>
 
 <style lang="postcss">
-  input[type='date']::-webkit-calendar-picker-indicator {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    top: 0;
-    height: auto;
-    width: auto;
-    cursor: pointer;
-    background: transparent;
-    color: transparent;
-  }
-  button {
+  .arrow-button {
     @apply h-full rounded hover:bg-neutral-50 active:bg-neutral-100;
   }
 </style>
