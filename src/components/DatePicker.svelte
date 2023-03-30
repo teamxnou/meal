08bcb9825c4fe512fd06e55346d552b7c20bb051
xnousnow <xnou.snow@gmail.com>
2023-03-30 17:41:19 +0900

    :lipstick: 달력 인터페이스 추가

--- src/components/DatePicker.svelte
+++ src/components/DatePicker.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { fade } from 'svelte/transition'
+  import { fade, slide } from 'svelte/transition'
   import { ChevronLeft, ChevronRight } from 'lucide-svelte'
 
   export let date: string, updateDate: (date: string) => void
@@ -43,10 +43,75 @@
       if (e.key === 'Escape') openDatepicker = false
     }}
   />
+  <div
+    class="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col gap-3 rounded-2xl bg-white p-4"
+    transition:slide={{ duration: 200 }}
+  >
+    <div class="flex items-center">
+      <button class="month-arrow-button">
+        <ChevronLeft class="h-7 w-7" />
+      </button>
+      <div class="flex-grow text-center text-lg font-semibold">2023년 3월</div>
+      <button class="month-arrow-button">
+        <ChevronRight class="h-7 w-7" />
+      </button>
+    </div>
+    <ul class="grid grid-cols-5 gap-3 text-center">
+      {#each ['월', '화', '수', '목', '금'] as day}
+        <li class="day-indicator">{day}</li>
+      {/each}
+      <li class="placeholder" />
+      <li class="placeholder" />
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>13</li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>20</li>
+      <li>21</li>
+      <li>22</li>
+      <li>23</li>
+      <li>24</li>
+      <li>27</li>
+      <li>28</li>
+      <li>29</li>
+      <li class="active">30</li>
+      <li class="today">31</li>
+    </ul>
+  </div>
 {/if}
 
 <style lang="postcss">
   .arrow-button {
     @apply h-full rounded hover:bg-neutral-50 active:bg-neutral-100;
+  }
+  .month-arrow-button {
+    @apply flex h-10 w-10 items-center justify-center rounded-full hover:bg-neutral-50 active:bg-neutral-100 text-green-700;
+  }
+  .day-indicator {
+    @apply cursor-default bg-transparent hover:bg-transparent text-sm text-neutral-400 h-3;
+  }
+  ul li {
+    @apply flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-[1.25rem] hover:bg-green-50 active:bg-green-100;
+  }
+  ul li.placeholder {
+    @apply cursor-default bg-transparent hover:bg-transparent;
+  }
+  ul li.active {
+    @apply bg-green-100 font-semibold text-green-700;
+  }
+  ul li.today {
+    @apply text-green-700;
+  }
+  ul li.active.today {
+    @apply bg-green-500 text-white;
   }
 </style>
