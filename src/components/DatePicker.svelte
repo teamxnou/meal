0223b8f8b145b82bc264f3733b4e439106b9b05f
xnousnow <xnou.snow@gmail.com>
2023-03-30 18:07:37 +0900

    :construction: 날짜 선택 구현

--- src/components/DatePicker.svelte
+++ src/components/DatePicker.svelte
@@ -2,7 +2,7 @@
   import { fade, slide } from 'svelte/transition'
   import { ChevronLeft, ChevronRight } from 'lucide-svelte'
 
-  export let date: string, updateDate: (date: string) => void
+  export let date: Date, updateDate: (date: Date) => void
 
   $: formattedDate = new Date(date).toLocaleDateString('ko-KR', {
     month: 'long',
@@ -13,11 +13,24 @@
   function changeDate(offset: number) {
     const newDate = new Date(date)
     newDate.setDate(newDate.getDate() + offset)
-    date = newDate.toISOString().slice(0, 10)
-    updateDate(date)
+    updateDate(newDate)
   }
 
   let openDatepicker = false
+  let year = date.getFullYear()
+  let month = date.getMonth() + 1
+  let selectedDate = new Date()
+  $: weekDaysInMonth = new Array(new Date(year, month, 0).getDate())
+    .fill(0)
+    .map((_, i) => i + 1)
+    .filter(
+      (day) =>
+        new Date(year, month - 1, day).getDay() !== 0 &&
+        new Date(year, month - 1, day).getDay() !== 6
+    )
+
+  $: placeholderDays = new Date(year, month - 1, 1).getDay() - 2
+  $: weekDayStart = placeholderDays < 0 ? 6 : placeholderDays
 </script>
 
 <div class="mb-16 flex items-center justify-center gap-1">
@@ -51,7 +64,7 @@
       <button class="month-arrow-button">
         <ChevronLeft class="h-7 w-7" />
       </button>
-      <div class="flex-grow text-center text-lg font-semibold">2023년 3월</div>
+      <div class="flex-grow text-center text-lg font-semibold">{year}년 {month}월</div>
       <button class="month-arrow-button">
         <ChevronRight class="h-7 w-7" />
       </button>
@@ -60,31 +73,24 @@
       {#each ['월', '화', '수', '목', '금'] as day}
         <li class="day-indicator">{day}</li>
       {/each}
-      <li class="placeholder" />
-      <li class="placeholder" />
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>10</li>
-      <li>13</li>
-      <li>14</li>
-      <li>15</li>
-      <li>16</li>
-      <li>17</li>
-      <li>20</li>
-      <li>21</li>
-      <li>22</li>
-      <li>23</li>
-      <li>24</li>
-      <li>27</li>
-      <li>28</li>
-      <li>29</li>
-      <li class="active">30</li>
-      <li class="today">31</li>
+      {#each Array(placeholderDays).fill(0) as _}
+        <li class="placeholder" />
+      {/each}
+      {#each weekDaysInMonth as day}
+        <li
+          class:active={day === date.getDate()}
+          class:today={day === selectedDate.getDate() &&
+            month === selectedDate.getMonth() + 1 &&
+            year === selectedDate.getFullYear()
+          }
+        >
+          <button on:click={() => {
+            const newDate = new Date(date)
+            newDate.setDate(day)
+            updateDate(newDate)
+          }}>{day}</button>
+        </li>
+      {/each}
     </ul>
   </div>
 {/if}
@@ -99,19 +105,19 @@
   .day-indicator {
     @apply cursor-default bg-transparent hover:bg-transparent text-sm text-neutral-400 h-3;
   }
-  ul li {
+  ul li button {
     @apply flex h-10 w-10 cursor-pointer items-center justify-center rounded-full text-[1.25rem] hover:bg-green-50 active:bg-green-100;
   }
-  ul li.placeholder {
+  ul li.placeholder button {
     @apply cursor-default bg-transparent hover:bg-transparent;
   }
-  ul li.active {
+  ul li.active button {
     @apply bg-green-100 font-semibold text-green-700;
   }
-  ul li.today {
+  ul li.today button {
     @apply text-green-700;
   }
-  ul li.active.today {
+  ul li.active.today button {
     @apply bg-green-500 text-white;
   }
 </style>
