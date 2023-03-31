ebcef5808804281c67721d5b606056788ed3ef94
xnousnow <xnou.snow@gmail.com>
2023-03-31 16:43:46 +0900

    :art: 코드 포맷

--- src/components/MenuBar.svelte
+++ src/components/MenuBar.svelte
@@ -23,7 +23,10 @@
   class:shadow-lg={!search}
 >
   {#if back}
-    <button on:click={() => history.back()} class="mr-3 hover:bg-black/5 active:bg-black/10 rounded">
+    <button
+      on:click={() => history.back()}
+      class="mr-3 rounded hover:bg-black/5 active:bg-black/10"
+    >
       <ChevronLeft class="h-7 w-7" />
     </button>
   {/if}
