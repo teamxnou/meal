b451b76f3515b2758b8b9d2525e5606c472eda16
xnousnow <xnou.snow@gmail.com>
2023-04-03 22:01:10 +0900

    :speech_balloon: 급식 정보를 가져오는 중 오류가 생기면 알림

--- src/components/MealList.svelte
+++ src/components/MealList.svelte
@@ -2,6 +2,9 @@
   import { onMount } from 'svelte'
   import { selectedCity, selectedSchool } from '../stores'
 
+  import { AlertCircle } from 'lucide-svelte'
+  import SimpleInfo from './SimpleInfo.svelte'
+
   export let date: Date
 
   $: formattedDate = `${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, '0')}${String(date.getDate()).padStart(2, '0')}`
@@ -15,6 +18,7 @@
     cityCode = value
   })
 
+  let error: boolean = false
   let meal: string[] = []
   function updateMeal() {
     console.log(`Updating meal for ${date}. School: ${schoolCode}, City: ${cityCode}`)
@@ -24,7 +28,14 @@
         import.meta.env.VITE_NEIS_API_KEY
       }&Type=json&ATPT_OFCDC_SC_CODE=${cityCode}&SD_SCHUL_CODE=${schoolCode}&MLSV_YMD=${formattedDate}`
     ).then((res) => res.json()).then((res) => {
+      if (!res.mealServiceDietInfo) {
+        error = true
+        return
+      }
       meal = res.mealServiceDietInfo[1].row[0].DDISH_NM.split('<br/>')
+      error = false
+    }).catch((err) => {
+      error = true
     })
   }
 
@@ -38,8 +49,18 @@
   }
 </script>
 
-<ul class="flex grow flex-col items-center justify-center text-3xl">
-  {#each meal as menu}
-    <li>{menu}</li>
-  {/each}
-</ul>
+{#if !error}
+  <ul class="flex grow flex-col items-center justify-center text-3xl">
+    {#each meal as menu}
+      <li>{menu}</li>
+    {/each}
+  </ul>
+{:else}
+  <div class="flex grow flex-col items-center justify-center">
+    <SimpleInfo
+      Icon={AlertCircle}
+      title="오류가 발생했어요"
+      description="급식 정보를 불러오는 도중 오류가 발생했어요. 다시 시도해주세요."
+    />
+  </div>
+{/if}
