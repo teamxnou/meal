a81e58e82d7adda097be4cccfe129a20942a2b73
xnousnow <xnou.snow@gmail.com>
2023-04-07 17:29:34 +0900

    :art: 코드 포맷

--- src/components/MealList.svelte
+++ src/components/MealList.svelte
@@ -7,7 +7,9 @@
 
   export let date: Date
 
-  $: formattedDate = `${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, '0')}${String(date.getDate()).padStart(2, '0')}`
+  $: formattedDate = `${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, '0')}${String(
+    date.getDate()
+  ).padStart(2, '0')}`
 
   let schoolCode: number
   selectedSchool.subscribe((value) => {
@@ -28,16 +30,19 @@
       `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${
         import.meta.env.VITE_NEIS_API_KEY
       }&Type=json&ATPT_OFCDC_SC_CODE=${cityCode}&SD_SCHUL_CODE=${schoolCode}&MLSV_YMD=${formattedDate}`
-    ).then((res) => res.json()).then((res) => {
-      if (!res.mealServiceDietInfo) {
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (!res.mealServiceDietInfo) {
+          error = true
+          return
+        }
+        meal = res.mealServiceDietInfo[1].row[0].DDISH_NM.split('<br/>')
+        error = false
+      })
+      .catch(() => {
         error = true
-        return
-      }
-      meal = res.mealServiceDietInfo[1].row[0].DDISH_NM.split('<br/>')
-      error = false
-    }).catch(() => {
-      error = true
-    })
+      })
   }
 
   onMount(() => {
@@ -63,7 +68,12 @@
       title="학교를 선택해주세요"
       description="학교를 선택하면 급식을 확인할 수 있어요."
     />
-    <a href="/school-selection" class="mb-2 rounded-lg text-white py-2 px-4 bg-green-500 active:bg-green-600">학교 선택하기</a>
+    <a
+      href="/school-selection"
+      class="mb-2 rounded-lg bg-green-500 py-2 px-4 text-white active:bg-green-600"
+    >
+      학교 선택하기
+    </a>
   </div>
 {:else}
   <div class="flex grow flex-col items-center justify-center">
