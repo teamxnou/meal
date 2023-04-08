92ba54fa31276daeab42df3b89700a70e76d52bb
xnousnow <xnou.snow@gmail.com>
2023-04-08 19:52:49 +0900

    :lipstick: `SimpleInfo`의 단어가 중간에서 줄바꿈되지 않게 변경

--- src/components/SimpleInfo.svelte
+++ src/components/SimpleInfo.svelte
@@ -2,7 +2,7 @@
   export let Icon: any, title: string, description: string;
 </script>
 
-<div class="flex flex-col gap-1 items-center text-center">
+<div class="flex flex-col gap-1 items-center text-center break-keep">
   <Icon class="h-32 w-32 mb-3" />
   <h1 class="text-2xl font-semibold">{title}</h1>
   <p>{description}</p>
