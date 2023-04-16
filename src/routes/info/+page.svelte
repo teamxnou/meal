dbe504c5697b14001ba4f6c30394a92958a27349
xnousnow <xnou.snow@gmail.com>
2023-04-16 15:00:23 +0900

    :construction: 임시 재료 정보 페이지 제작

---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  import vegetableData from '../../vegetableData'
+
+  const id = parseInt($page.url.searchParams.get('id') || '')
+  const isIdNaN = isNaN(id)
+</script>
+
+{#if !isIdNaN}
+  info id: {id}<br />
+  object: {JSON.stringify(vegetableData[id])}
+{:else if id}
+  id is not a number
+{:else}
+  id is not set
+{/if}
