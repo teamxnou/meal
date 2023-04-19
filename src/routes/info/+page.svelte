e6e7de94d2cf1e5a3bb5bafefac997ac2f73a76d
xnousnow <xnou.snow@gmail.com>
2023-04-19 19:16:43 +0900

    :sparkles: 재료 정보 화면 구현

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
   import { page } from '$app/stores'
 
+  import MenuBar from '../../components/MenuBar.svelte'
+
   import vegetableData from '../../vegetableData'
 
   const id = parseInt($page.url.searchParams.get('id') || '')
   const isIdNaN = isNaN(id)
+
+  const vegetable = vegetableData[id]
 </script>
 
-{#if !isIdNaN}
-  info id: {id}<br />
-  object: {JSON.stringify(vegetableData[id])}
-{:else if id}
-  id is not a number
-{:else}
-  id is not set
-{/if}
+<div class="flex max-h-screen grow flex-col bg-neutral-300">
+  <MenuBar title="재료 정보" back={true} />
+  {#if id && !isIdNaN}
+    <div class="flex grow flex-col">
+      <div class="grow flex snap-x snap-mandatorny overflow-x-scroll w-screen">
+        <div class="grow flex w-[300vw] snap-x snap-mandatory snap-start">
+          {#each vegetable.images as image, i}
+            <div class="h-[calc(100vh-54px-8rem)] min-w-screen max-w-screen w-screen">
+              <!-- Had to use calc, it was hard to make it with only %'s. -->
+              <img src={image} class="object-contain snap-start h-[calc(100vh-54px-8rem)] w-screen" alt={`${vegetable.name}의 ${i + 1}번째 사진`} />
+            </div>
+          {/each}
+        </div>
+      </div>
+      <div class="rounded-t-lg bg-white p-5 h-32">
+        <h1 class="text-2xl font-semibold">{vegetable.name}</h1>
+        <p>{vegetable.description}</p>
+      </div>
+    </div>
+  {/if}
+</div>
