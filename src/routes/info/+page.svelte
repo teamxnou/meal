8b36da090c18a0095e5f7d2e35321e717c3b4932
xnousnow <xnou.snow@gmail.com>
2023-04-19 20:41:46 +0900

    :speech_balloon: 재료 정보를 찾을 수 없을 때 알림

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
   import { page } from '$app/stores'
+  
+  import { AlertCircle } from 'lucide-svelte'
 
   import MenuBar from '../../components/MenuBar.svelte'
+  import SimpleInfo from '../../components/SimpleInfo.svelte'
 
   import vegetableData from '../../vegetableData'
 
   const id = parseInt($page.url.searchParams.get('id') || '')
-  const isIdNaN = isNaN(id)
+  const isIdValid = id && !isNaN(id) && id >= 0 && id < vegetableData.length
 
   const vegetable = vegetableData[id]
 </script>
 
-<div class="flex max-h-screen grow flex-col bg-neutral-300">
+<div class="flex max-h-screen grow flex-col" class:bg-neutral-300={isIdValid}>
   <MenuBar title="재료 정보" back={true} />
-  {#if id && !isIdNaN}
+  {#if id && isIdValid}
     <div class="flex grow flex-col">
       <div class="grow flex snap-x snap-mandatorny overflow-x-scroll w-screen">
         <div class="grow flex w-[300vw] snap-x snap-mandatory snap-start">
@@ -29,6 +32,10 @@
         <h1 class="text-2xl font-semibold">{vegetable.name}</h1>
         <p>{vegetable.description}</p>
       </div>
+    </div>
+  {:else}
+    <div class="flex-grow flex flex-col items-center justify-center">
+      <SimpleInfo Icon={AlertCircle} title="재료 정보를 찾을 수 없어요" description="무언가 잘못되었어요." />
     </div>
   {/if}
 </div>
