7726725070dde3a27e78f52caab9c0cdede8932d
xnousnow <xnou.snow@gmail.com>
2023-04-22 10:20:19 +0900

    :chart_with_upwards_trend: `@vercel/analytics` 패키지 설치 및 설정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
   import '../app.css'
+  
+  // Vercel analytics
+  import { dev } from '$app/environment'
+  import { inject } from '@vercel/analytics'
+  inject({ mode: dev ? 'development' : 'production' })
+
   import { selectedSchoolName, openSchoolToast } from '../stores'
 
   import Toast from '../components/Toast.svelte'
