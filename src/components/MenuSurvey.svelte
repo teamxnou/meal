4dc8dae1b64e2dc21bcf104ae64ba47fce1ebfad
xnousnow <xnou.snow@gmail.com>
2023-05-05 18:53:26 +0900

    :art: 코드 포맷

--- src/components/MenuSurvey.svelte
+++ src/components/MenuSurvey.svelte
@@ -51,7 +51,7 @@
   if (typeof window !== 'undefined' && localStorage.getItem('lastSurveyDate') != formattedDate) {
     fetchYesterdayMeal()
   }
-  
+
   function submitSurvey() {
     meal.forEach(async (menu) => {
       const { data } = await supabase
