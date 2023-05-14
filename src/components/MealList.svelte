2ceb6a3ae1b251198f5ef2d440bfd78b28c4adf3
xnousnow <xnou.snow@gmail.com>
2023-05-14 14:15:46 +0900

    :wheelchair: 학교 선택하기 버튼의 `role`을 버튼으로 설정

--- src/components/MealList.svelte
+++ src/components/MealList.svelte
@@ -128,6 +128,7 @@
       <a
         href="/school-selection"
         class="mb-2 rounded-lg bg-green-500 py-2 px-4 text-white active:bg-green-600"
+        role="button"
       >
         학교 선택하기
       </a>
