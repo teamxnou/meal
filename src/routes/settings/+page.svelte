040ec6418b955d8e3cdd052d0870c7a0b05d4e34
xnousnow <xnou.snow@gmail.com>
2023-05-18 19:51:05 +0900

    :wheelchair: 설정 토글 접근성 개선

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -23,13 +23,15 @@
   <ul class="divide-y overflow-hidden rounded-lg">
     <li>
       <label
-        class="flex cursor-pointer items-center justify-between bg-white px-5 py-1 hover:bg-neutral-50"
+        class="flex cursor-pointer items-center justify-between bg-white px-5 py-1 hover:bg-neutral-50 relative"
       >
-        <span class="my-2">메뉴 설문 참가</span>
+        <span class="my-2" aria-hidden="true">메뉴 설문 참가</span>
         <input
           type="checkbox"
-          class="peer appearance-none"
+          class="peer appearance-none absolute top-0 left-0 right-0 bottom-0 w-full h-full"
           bind:checked={_settings.parcipiateMenuSurvey}
+          role="switch"
+          aria-label="메뉴 설문 참가"
         />
         <div
           class="group-9 w-14 rounded-full border-2 bg-neutral-50 transition duration-500 peer-checked:border-green-500 peer-checked:bg-green-500 peer-checked:[&>div]:translate-x-5"
@@ -40,17 +42,21 @@
     </li>
     <li>
       <label
-        class="flex cursor-pointer items-center justify-between bg-white px-5 py-1 hover:bg-neutral-50"
+        class="flex cursor-pointer items-center justify-between bg-white px-5 py-1 hover:bg-neutral-50 relative"
       >
-        <div class="flex flex-col my-2">
-          <span>메뉴 설문 결과 보기</span>
-          <p class="text-neutral-400 text-xs">메뉴 설문에서 다른 학생들이 만족스럽다고 표시한 메뉴에 별을 표시해요.</p>
-        </div>
         <input
           type="checkbox"
-          class="peer appearance-none"
+          class="peer appearance-none absolute top-0 left-0 right-0 bottom-0 w-full h-full"
           bind:checked={_settings.viewMenuSurvey}
+          role="switch"
+          aria-label="메뉴 설문 결과 보기"
         />
+        <div class="my-2 flex flex-col">
+          <span aria-hidden="true">메뉴 설문 결과 보기</span>
+          <p class="text-xs text-neutral-400">
+            메뉴 설문에서 다른 학생들이 만족스럽다고 표시한 메뉴에 별을 표시해요.
+          </p>
+        </div>
         <div
           class="group-9 w-14 rounded-full border-2 bg-neutral-50 transition duration-500 peer-checked:border-green-500 peer-checked:bg-green-500 peer-checked:[&>div]:translate-x-5"
         >
