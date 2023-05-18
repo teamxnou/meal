5e3c0025dcf7dd706a800ea2dbe4bcb11519ef68
xnousnow <xnou.snow@gmail.com>
2023-05-18 20:49:26 +0900

    :art: 설정의 토글을 컴포넌트로 분리

---
+++ src/components/MenuSwitch.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let name: string,
+    description: string = '',
+    checked: boolean,
+    updateChecked: (checked: boolean) => void;
+</script>
+
+<label
+  class="relative flex cursor-pointer items-center justify-between bg-white px-5 py-1 hover:bg-neutral-50"
+>
+  <input
+    type="checkbox"
+    class="peer absolute top-0 left-0 right-0 bottom-0 h-full w-full appearance-none"
+    bind:checked={checked}
+    on:change={() => updateChecked(checked)}
+    role="switch"
+    aria-label={name}
+  />
+  <div class="my-2 flex flex-col">
+    <span aria-hidden="true">{name}</span>
+    <p class="text-xs text-neutral-400" aria-hidden={description ? 'false' : 'true'}>
+      {description}
+    </p>
+  </div>
+  <div
+    class="group-9 w-14 rounded-full border-2 bg-neutral-50 transition duration-500 peer-checked:border-green-500 peer-checked:bg-green-500 peer-checked:[&>div]:translate-x-5"
+  >
+    <div class="h-8 w-8 transform rounded-full bg-white shadow transition duration-300" />
+  </div>
+</label>
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -3,6 +3,7 @@
   import type { Settings } from '../../settings'
 
   import MenuBar from '../../components/MenuBar.svelte'
+  import MenuSwitch from '../../components/MenuSwitch.svelte'
 
   let _settings: Settings = $settings
 
@@ -22,47 +23,19 @@
 <div class="flex h-full grow flex-col gap-3 bg-neutral-100 p-4">
   <ul class="divide-y overflow-hidden rounded-lg">
     <li>
-      <label
-        class="flex cursor-pointer items-center justify-between bg-white px-5 py-1 hover:bg-neutral-50 relative"
-      >
-        <span class="my-2" aria-hidden="true">메뉴 설문 참가</span>
-        <input
-          type="checkbox"
-          class="peer appearance-none absolute top-0 left-0 right-0 bottom-0 w-full h-full"
-          bind:checked={_settings.parcipiateMenuSurvey}
-          role="switch"
-          aria-label="메뉴 설문 참가"
-        />
-        <div
-          class="group-9 w-14 rounded-full border-2 bg-neutral-50 transition duration-500 peer-checked:border-green-500 peer-checked:bg-green-500 peer-checked:[&>div]:translate-x-5"
-        >
-          <div class="h-8 w-8 transform rounded-full bg-white shadow transition duration-300" />
-        </div>
-      </label>
+      <MenuSwitch
+        name="메뉴 설문 참가"
+        checked={_settings.parcipiateMenuSurvey}
+        updateChecked={(checked) => (_settings.parcipiateMenuSurvey = checked)}
+      />
     </li>
     <li>
-      <label
-        class="flex cursor-pointer items-center justify-between bg-white px-5 py-1 hover:bg-neutral-50 relative"
-      >
-        <input
-          type="checkbox"
-          class="peer appearance-none absolute top-0 left-0 right-0 bottom-0 w-full h-full"
-          bind:checked={_settings.viewMenuSurvey}
-          role="switch"
-          aria-label="메뉴 설문 결과 보기"
-        />
-        <div class="my-2 flex flex-col">
-          <span aria-hidden="true">메뉴 설문 결과 보기</span>
-          <p class="text-xs text-neutral-400">
-            메뉴 설문에서 다른 학생들이 만족스럽다고 표시한 메뉴에 별을 표시해요.
-          </p>
-        </div>
-        <div
-          class="group-9 w-14 rounded-full border-2 bg-neutral-50 transition duration-500 peer-checked:border-green-500 peer-checked:bg-green-500 peer-checked:[&>div]:translate-x-5"
-        >
-          <div class="h-8 w-8 transform rounded-full bg-white shadow transition duration-300" />
-        </div>
-      </label>
+      <MenuSwitch
+        name="메뉴 설문 결과 보기"
+        description="메뉴 설문에서 다른 학생들이 만족스럽다고 표시한 메뉴에 별을 표시해요."
+        bind:checked={_settings.viewMenuSurvey}
+        updateChecked={(checked) => (_settings.viewMenuSurvey = checked)}
+      />
     </li>
   </ul>
   <ul class="overflow-hidden rounded-lg">
