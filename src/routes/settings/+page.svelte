9d5434a59c25fa75abac9373a2b4327561f45d6d
xnousnow <xnou.snow@gmail.com>
2023-05-22 21:08:26 +0900

    :construction: 기능 추가 업데이트가 있을 때만 알림이 뜨도록 수정

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { notifyRelease } from '../../stores'
   import { settings } from '../../settings'
   import type { Settings } from '../../settings'
 
@@ -24,29 +25,31 @@
 
 <MenuBar title="설정" back={true} />
 <div class="flex h-full grow flex-col gap-3 bg-neutral-100 p-4">
-  <div class="flex flex-col items-center rounded-lg bg-white">
-    <span class="sr-only">업데이트 정보</span>
-    <div
-      class="w-full rounded-t-lg bg-gradient-to-br from-blue-500 to-purple-600 p-8 text-center text-white"
-    >
-      <h1 class="font-mono text-3xl font-bold">v{version}</h1>
+  {#if $notifyRelease}
+    <div class="flex flex-col items-center rounded-lg bg-white">
+      <span class="sr-only">업데이트 정보</span>
+      <div
+        class="w-full rounded-t-lg bg-gradient-to-br from-blue-500 to-purple-600 p-8 text-center text-white"
+      >
+        <h1 class="font-mono text-3xl font-bold">v{version}</h1>
+      </div>
+      <div class="w-full">
+        <ul class="flex w-full flex-col gap-2 p-4">
+          {#each [Bird, Dog, Cat, Fish] as Icon}
+            <li class="flex w-full items-center gap-5 px-3">
+              <div>
+                <Icon class="h-8 w-8 text-purple-500" />
+              </div>
+              <div class="grow">
+                <h3 class="font-semibold">Example title</h3>
+                <p class="font-normal text-neutral-400">Example description</p>
+              </div>
+            </li>
+          {/each}
+        </ul>
+      </div>
     </div>
-    <div class="w-full">
-      <ul class="flex w-full flex-col gap-2 p-4">
-        {#each [Bird, Dog, Cat, Fish] as Icon}
-          <li class="flex w-full items-center gap-5 px-3">
-            <div>
-              <Icon class="h-8 w-8 text-purple-500" />
-            </div>
-            <div class="grow">
-              <h3 class="font-semibold">Example title</h3>
-              <p class="font-normal text-neutral-400">Example description</p>
-            </div>
-          </li>
-        {/each}
-      </ul>
-    </div>
-  </div>
+  {/if}
   <ul class="divide-y overflow-hidden rounded-lg">
     <li>
       <MenuSwitch
